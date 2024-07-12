84fcc2a1b5d93a18c5e2f84b95dfb81348728046
HyunsukChoo <cngustjr1235@snu.ac.kr>
2024-07-12 18:01:39 +0900

    interpolate data

--- Beninca_vertical/Step0_interpolation_and_cut.m
+++ Beninca_vertical/Step0_interpolation_and_cut.m
@@ -19,7 +19,7 @@ overlapping_ratio = 0.9;  % overlapping ratio of moving window technique
 % window_size_ori/time_interval becomes number of time points per cutted data
 % window_size_ori/time_interval = 100 is recommended
 % window_size_ori/time_interval is high (low) make the inference accurate (less accurate) and slow (fast)
-time_interval = 1/4;       
+time_interval = 1;       
 
 %% process parameters
 window_size = window_size_ori / time_interval;
@@ -39,33 +39,13 @@ ylim([0,1])
 yticks([])
 
 %% interpolate data
-t_fit = linspace(0,t(end),length(t)/time_interval+1).';
+spline_param = 0.01;
+
+t_fit = (t_ori(1):time_interval:t_ori(end)).';
 y_fit = zeros(length(t_fit),num_component);
 
-if method == 1 % linear
-    for i = 1:num_component
-        y_fit(:,i) = interp1(t, y(:,i),t_fit,'linear');
-    end
-elseif method == 2 % spline
-    for i = 1:num_component
-        y_fit(:,i) = interp1(t, y(:,i),t_fit,'spline');
-    end
-elseif method == 3 % fourier
-    for i = 1:num_component
-        y_fit(:,i) = interp1(t, y(:,i),t_fit,'linear');
-    end
-    option = ['fourier',num2str(num_fourier)];
-    for i = 1:num_component
-        fitting = fit(t_fit,y_fit(:,i),option);
-        w = fitting.w;
-        fouriers = ones(1,length(t_fit));
-        for j = 1:num_fourier
-            fouriers = [fouriers; cos(j*w*t_fit.')];
-            fouriers = [fouriers; sin(j*w*t_fit.')];
-        end
-        coeffs = coeffvalues(fitting);
-        y_fit(:,i) = coeffs(1:end-1) * fouriers;
-    end
+for i = 1:num_component
+	y_fit(:,i) = csaps(t_ori, y_ori(:, i), spline_param, t_fit);
 end
 
 %% plot interpolated data
@@ -74,12 +54,12 @@ for i = 1:num_component
     plot(t_fit, y_fit(:,i))
     hold on
 end
-xlim([0,t_fit(end)])
 
 xlim([0,t_fit(end)])
 xticks([])
 ylim([0,1.02])
 yticks([])
+
 %% cut and interpolate the data
 y_total = {};
 start = 1; % it can be changed if users do not want to use the beginning of time series
