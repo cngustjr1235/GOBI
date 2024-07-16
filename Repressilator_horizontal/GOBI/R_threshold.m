bcc9b445bd458cdd5ea3af5e40b7c7966653c6f6
HyunsukChoo <cngustjr1235@snu.ac.kr>
2024-07-16 11:05:16 +0900

    each component divided into 2

--- Repressilator_horizontal/GOBI/R_threshold.m
+++ Repressilator_horizontal/GOBI/R_threshold.m
@@ -1,7 +1,9 @@
 function [R_tmp] = R_threshold(R, thres)
 R_tmp = R;
 for i = 1:length(R)
-    if R(i) < thres
+    if isnan(R(i))
+        R_tmp(i) = 0;
+    elseif R(i) < thres
         R_tmp(i) = 0;
     else
         R_tmp(i) = 1;
